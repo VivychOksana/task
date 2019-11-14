e3ca15712d5fa044266438b162773995875851de
Vivych Oksana <vivichokcana@gmail.com>
2019-11-15 01:25:06 +0200

    Updates

---
+++ precache-manifest.b812c52b3a0fc50b6bbca4848a5cf0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa82035fe68e0ed5f3663f2ee43e18d",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "365abbf003bbfcd30dbb",
+    "url": "/task/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2dd273868087964bb0cd",
+    "url": "/task/static/js/2.8f5ded0e.chunk.js"
+  },
+  {
+    "revision": "365abbf003bbfcd30dbb",
+    "url": "/task/static/js/main.82d96697.chunk.js"
+  },
+  {
+    "revision": "79cff03d96a1a2eeb3a0",
+    "url": "/task/static/js/runtime-main.5c353989.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/task/static/media/logo.25bf045c.svg"
+  }
+]);
